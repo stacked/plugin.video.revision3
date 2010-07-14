99b9ae04f424b9de17fa0366357461b31fc42543
stacked <stacked.xbmc@gmail.com>
2010-07-13 19:48:30 -0700

    Revision3 v1.0.0

--- default.py
+++ default.py
@@ -1,10 +1,30 @@
+#/*
+# *      Copyright (C) 2010 Team XBMC
+# *
+# *
+# *  This Program is free software; you can redistribute it and/or modify
+# *  it under the terms of the GNU General Public License as published by
+# *  the Free Software Foundation; either version 2, or (at your option)
+# *  any later version.
+# *
+# *  This Program is distributed in the hope that it will be useful,
+# *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+# *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# *  GNU General Public License for more details.
+# *
+# *  You should have received a copy of the GNU General Public License
+# *  along with this program; see the file COPYING.  If not, write to
+# *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+# *  http://www.gnu.org/copyleft/gpl.html
+# *
+# */
 
 import xbmc, xbmcgui, xbmcplugin, urllib2, urllib, re, string, sys, os, traceback, xbmcaddon
 
 __plugin__ =  'Revision3'
 __author__ = 'stacked <stacked.xbmc@gmail.com>'
 __url__ = 'http://code.google.com/p/plugin/'
-__date__ = '07-12-2010'
+__date__ = '07-13-2010'
 __version__ = '1.0.0'
 __settings__ = xbmcaddon.Addon(id='plugin.video.revision3')
 
